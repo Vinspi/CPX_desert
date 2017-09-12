03f15c570a1f3169f5a1ab40911dc454b8201c8a
Unknown <lucasl.pro13@gmail.com>
2017-09-12 22:33:38 +0200

    Ajout fontionnalités annexes
    Initialisation de la structure matricielle.
    Ouverture du fichier et chargement de la matrice. Ecriture (representation) de la matrice dans un fichier.

--- cplx_graph_desert.c
+++ cplx_graph_desert.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*	Liste des codes d'erreur
+
+	10 : L'ouverture du fichier source a échoué
+	15 : L'ouverture du fichier de sortie à échoué
+
+*/
+
 #define n_max 1000
 
 typedef int sommet;
-
 
 typedef struct {
 	int a[n_max][n_max];
@@ -19,11 +26,107 @@ typedef struct maillon{
 
 typedef maillon *liste;
 
+
+
+/*	Fontion initGraphe : initialise les champs de la structure representant le graphe en mémoire
+	[Param IN]	Graph_m *g : pointeur vers la structure reprensentant le graphe en mémoire */
+
+void initGraph(Graph_m *g, int n){
+	for(int i=0; i<n_max; i++){
+		for(int j=0; j<n_max; j++){
+			g->a[i][j] = 0;
+		}
+		g->degre[i] = 0;
+	}
+	g->n = n;
+}
+
+
+/* Fonction printGraph : écrit la représentation matricielle du graphe dans un fichier
+	[Param IN]	char *filename : le nom du fichier dans lequel écrire  
+	[Param IN]	Graph_m *g : pointeur vers la structure reprensentant le graphe en mémoire */
+
+void printGraph(char* filename, Graph_m *g){
+	FILE *out = NULL;
+	out = fopen(filename, "w+");
+
+	if(out == NULL)
+		exit(15);
+
+	fprintf(out, "        |");
+
+	for(int i=0; i<g->n; i++)
+		fprintf(out, "s%d |", i);
+
+	fprintf(out, "\n");
+
+	for(int t=0; t<g->n+2; t++){
+			fprintf(out, "----");
+	}	
+
+	for(int i=0; i<g->n; i++){
+		fprintf(out, "\n s%d     | ", i);
+		for(int j=0; j<g->n; j++){
+			fprintf(out, "%d | ", g->a[i][j]);
+		}
+		fprintf(out, "\n");
+		for(int t=0; t<g->n+2; t++){
+			fprintf(out, "----");
+		}	
+	}
+
+	fclose(out);
+	printf("\n[Ecriture fichier terminée : %s]\n", filename);
+}
+
+
+/*	Fontion loadSource : construit le graphe (representation matricielle) à partir du fichier 
+	[Param IN]	char *filename : le nom du fichier contenant la liste des aretes 
+	[Param IN]	Graph_m *g : pointeur vers la structure reprensentant le graphe en mémoire */
+
+void loadSource(char* filename, Graph_m *g){
+
+	FILE *source = NULL;
+	source = fopen(filename, "r");
+	
+	if(source == NULL)
+		exit(10);
+   
+	char nbSt_str[10], nbAr_str[10], from_str[10], to_str[10];
+	int nbSt, nbAr, from, to;
+   
+	fscanf(source, "%s %s", nbSt_str, nbAr_str);
+	
+	nbSt = atoi(nbSt_str);
+	nbAr = atoi(nbAr_str);
+
+	initGraph(g, nbSt);
+   
+    printf("Nombre de sommets : %d\n", nbSt);
+	printf("Nombre d'aretes : %d\n", nbAr);
+
+	while(fscanf(source, "%s %s", from_str, to_str) != EOF){
+		from = atoi(from_str);
+		to = atoi(to_str);
+
+		g->a[from][to] = 1;
+		g->degre[from] = g->degre[from] + 1;
+
+		printf("( %d,", from);
+		printf(" %d )\n", to);
+	}
+
+	fclose(source);	
+}
+
 int main(void){
 
+	Graph_m *graph = malloc(sizeof(Graph_m));
+ 
+	loadSource("./source_graph", graph);
 	
-
-
+	printGraph("./matri_graph", graph);
+	
 	return 0;
 }
 
@@ -34,6 +137,3 @@ int verification_graphe_desert(liste x){
 
 	return 0;
 }
-
-
-
