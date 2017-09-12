7e43cd95f4307acc4fcca4421e3319c07c247814
Vinspi <vinspi13@gmail.com>
2017-09-12 18:37:41 +0200

    Add files via upload

---
+++ cplx_graph_desert.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n_max 1000
+
+typedef int sommet;
+
+
+typedef struct {
+	int a[n_max][n_max];
+	int n;
+	int degre[n_max];
+} Graph_m;
+
+typedef struct maillon{
+	sommet st;
+	struct maillon *suiv;
+} maillon;
+
+typedef maillon *liste;
+
+int main(void){
+
+	
+
+
+	return 0;
+}
+
+
+int verification_graphe_desert(liste x){
+
+
+
+	return 0;
+}
+
+
+
