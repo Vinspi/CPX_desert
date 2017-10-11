becbecd27c0d47de0a5bf13c5c9cbd509a1e7bd5
Dae <daeroufle@gmail.com>
2017-10-11 15:58:51 +0200

    Manque Makefile, et peut-être un dernier coup d'embelissement

--- calcul_maximal.c
+++ calcul_maximal.c
@@ -1,5 +1,171 @@
-#include "util.h"
+//#include "util.h"
 
+//Trucs à enlever quand quelqu'un fera un makefile. Qui marche.
+//___________________________________________________________________________
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define n_max 1000
+
+typedef int sommet;
+
+typedef struct {
+	int a[n_max][n_max];
+	int n;
+	int degre[n_max];
+} Graph;
+
+typedef struct maillon{
+	sommet st;
+	struct maillon *suiv;
+} maillon;
+
+typedef maillon *liste;
+
+
+//Fontion initGraphe : initialise les champs de la structure representant le graphe en mémoire
+//[Param IN]	Graph *g : pointeur vers la structure reprensentant le graphe en mémoire
+
+void initGraph(Graph *g, int n){
+	for(int i=0; i<n_max; i++){
+		for(int j=0; j<n_max; j++){
+			g->a[i][j] = 0;
+		}
+		g->degre[i] = 0;
+	}
+	g->n = n;
+}
+
+
+//Fonction printGraph : écrit la représentation matricielle du graphe dans un fichier
+//[Param IN]	char *filename : le nom du fichier dans lequel écrire
+//[Param IN]	Graph *g : pointeur vers la structure reprensentant le graphe en mémoire
+
+
+void printGraph(char* filename, Graph *g){
+	FILE *out = NULL;
+	out = fopen(filename, "w+");
+
+	if(out == NULL)
+		exit(15);
+
+	fprintf(out, "        |");
+
+	for(int i=0; i<g->n; i++)
+		fprintf(out, "s%d |", i);
+
+	fprintf(out, "\n");
+
+	for(int t=0; t<g->n+2; t++){
+			fprintf(out, "----");
+	}
+
+	for(int i=0; i<g->n; i++){
+		fprintf(out, "\n s%d     | ", i);
+		for(int j=0; j<g->n; j++){
+			fprintf(out, "%d | ", g->a[i][j]);
+		}
+		fprintf(out, "\n");
+		for(int t=0; t<g->n+2; t++){
+			fprintf(out, "----");
+		}
+	}
+
+	fclose(out);
+	printf("\n[Ecriture fichier terminée : %s]\n", filename);
+}
+
+
+//Fontion loadSource : construit le graphe (representation matricielle) à partir du fichier
+//[Param IN]	char *filename : le nom du fichier contenant la liste des aretes
+//[Param IN]	Graph *g : pointeur vers la structure reprensentant le graphe en mémoire
+
+void loadSource(char* filename, Graph *g){
+
+	FILE *source = NULL;
+	source = fopen(filename, "r");
+
+	if(source == NULL)
+		exit(10);
+
+	char nbSt_str[10], nbAr_str[10], from_str[10], to_str[10];
+	int nbSt, nbAr, from, to;
+
+	fscanf(source, "%s %s", nbSt_str, nbAr_str);
+
+	nbSt = atoi(nbSt_str);
+	nbAr = atoi(nbAr_str);
+
+	initGraph(g, nbSt);
+
+    printf("Nombre de sommets : %d\n", nbSt);
+	printf("Nombre d'aretes : %d\n", nbAr);
+
+	while(fscanf(source, "%s %s", from_str, to_str) != EOF){
+		from = atoi(from_str);
+		to = atoi(to_str);
+
+		if(g->a[from][to] != 1 && g->a[to][from] != 1){
+			g->degre[from] = g->degre[from] + 1;
+			g->degre[to] = g->degre[to] + 1;
+
+		}
+		if(g->a[from][to] != 1){
+			g->a[from][to] = 1;
+		}
+
+		if(g->a[to][from] != 1){
+			g->a[to][from] = 1;
+		}
+
+
+
+
+	}
+
+	fclose(source);
+}
+
+void printf_liste(liste x){
+
+	liste tmp = x; 	int i = 0;
+	printf("[ ");
+	while(tmp != NULL){
+		printf("%d ",tmp->st);
+		tmp = tmp->suiv;
+		i++;
+	}
+	printf("], taille %d.\n",i);
+}
+
+//Ajoute un sommet a à la liste x.
+liste add_to_liste(liste x, sommet a){
+	liste tmp = malloc(sizeof(liste));
+	tmp->st = a;
+	tmp->suiv = x;
+	return tmp;
+}
+
+void free_liste(liste x){
+
+	while(x != NULL){
+		liste tmp = x;
+		x = x -> suiv;
+		free(tmp);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+//___________________________________________________________________________
 
 
 //Fonction vérifiant si l'on a construit un maximal en observant la table d'adjacence : si tout les sommets sont marqués, on a créé un mmaximal.
@@ -7,7 +173,7 @@
 int verif_maximal(Graph *g, int* tab){
 
 	for(int i = 0; i < g->n; i++){
-		if(tab[i] == 1) return 0;
+		if(tab[i] == 0) return 0;
 	}
 	return 1;
 
@@ -18,38 +184,34 @@ int verif_maximal(Graph *g, int* tab){
 liste calcul_maximal(Graph *g){
 
     //Création de la table d'adjacence et d'un tampon.
-    int tableau_adjacence[n_max], i = 0;
-    for(int i = 0; i < g->n; i++);
-      tableau_adjacence[i] = 1;
-
+    int tableau_adjacence[g->n], i = 0;
+    for(int i = 0; i < g->n; i++){
+      tableau_adjacence[i] = 0;
+		}
 
 	  liste X = NULL;
-	  liste tmp_parcour = X;
 
     //Tant que l'on a pas créé de maximal, on continue :
-	  while(verif_maximal(g, all) != 1){
+	  while(verif_maximal(g, tableau_adjacence) != 1){
 		  i = 0;
 
-		  //Choisir le nouveau sommet : on choisira le premier sommet non-marqué que l'on trouve en parcourant la table d'adjacence.
-		  while(i < g->n && tableau_adjacence[i] == 0){
+		  //On choisit le nouveau sommet : on choisira le premier sommet non-marqué que l'on trouve en parcourant la table d'adjacence.
+		  while(i < g->n-1 && tableau_adjacence[i] == 1){
 			     i++;
 		  }
 
-      //On l'ajoute à la liste.
+
+      //On l'ajoute ensuite à la liste.
 		  X = add_to_liste(X, i);
-		  tmp_parcour = X;
 
       //Et on marque son adjacence.
-      while (tmp_parcour != NULL) {
+      tableau_adjacence[i] = 1;
+      for(int j=0;j<g->n;j++){
+				    if(g->a[i][j]){
+            	tableau_adjacence[j] = 1;
 
-        tableau_adjacence[tmp_parcour->st] = 0;
-        for(int j=0;j<g->n;j++){
-				      if(g->a[tmp_parcour->st][j])
-                tableau_adjacence[j] = 0;
-			  }
-        tmp_parcour = tmp_parcour -> suiv;
+						}
 		  }
-
 
 
 	   }
@@ -64,11 +226,11 @@ liste calcul_maximal(Graph *g){
 
 int main(int argc, char *argv[]){
 
-    if(argc != 1){
+    if(argc != 2){
       printf("USAGE : ./verif_desert fichier_contenant_le_graphe\n");
       exit(-1);
     }
-    printf("argv[1] = %s\n",argv[1]);
+    
     //Création du graphe.
     Graph *graph = malloc(sizeof(Graph));
     char* fgraph = argv[1];
--- verif_maximal.c
+++ verif_maximal.c
@@ -1,5 +1,171 @@
-#include "util.h"
+//#include "util.h"
 
+//Trucs à enlever quand quelqu'un fera un makefile. Qui marche.
+//___________________________________________________________________________
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define n_max 1000
+
+typedef int sommet;
+
+typedef struct {
+	int a[n_max][n_max];
+	int n;
+	int degre[n_max];
+} Graph;
+
+typedef struct maillon{
+	sommet st;
+	struct maillon *suiv;
+} maillon;
+
+typedef maillon *liste;
+
+
+//Fontion initGraphe : initialise les champs de la structure representant le graphe en mémoire
+//[Param IN]	Graph *g : pointeur vers la structure reprensentant le graphe en mémoire
+
+void initGraph(Graph *g, int n){
+	for(int i=0; i<n_max; i++){
+		for(int j=0; j<n_max; j++){
+			g->a[i][j] = 0;
+		}
+		g->degre[i] = 0;
+	}
+	g->n = n;
+}
+
+
+//Fonction printGraph : écrit la représentation matricielle du graphe dans un fichier
+//[Param IN]	char *filename : le nom du fichier dans lequel écrire
+//[Param IN]	Graph *g : pointeur vers la structure reprensentant le graphe en mémoire
+
+
+void printGraph(char* filename, Graph *g){
+	FILE *out = NULL;
+	out = fopen(filename, "w+");
+
+	if(out == NULL)
+		exit(15);
+
+	fprintf(out, "        |");
+
+	for(int i=0; i<g->n; i++)
+		fprintf(out, "s%d |", i);
+
+	fprintf(out, "\n");
+
+	for(int t=0; t<g->n+2; t++){
+			fprintf(out, "----");
+	}
+
+	for(int i=0; i<g->n; i++){
+		fprintf(out, "\n s%d     | ", i);
+		for(int j=0; j<g->n; j++){
+			fprintf(out, "%d | ", g->a[i][j]);
+		}
+		fprintf(out, "\n");
+		for(int t=0; t<g->n+2; t++){
+			fprintf(out, "----");
+		}
+	}
+
+	fclose(out);
+	printf("\n[Ecriture fichier terminée : %s]\n", filename);
+}
+
+
+//Fontion loadSource : construit le graphe (representation matricielle) à partir du fichier
+//[Param IN]	char *filename : le nom du fichier contenant la liste des aretes
+//[Param IN]	Graph *g : pointeur vers la structure reprensentant le graphe en mémoire
+
+void loadSource(char* filename, Graph *g){
+
+	FILE *source = NULL;
+	source = fopen(filename, "r");
+
+	if(source == NULL)
+		exit(10);
+
+	char nbSt_str[10], nbAr_str[10], from_str[10], to_str[10];
+	int nbSt, nbAr, from, to;
+
+	fscanf(source, "%s %s", nbSt_str, nbAr_str);
+
+	nbSt = atoi(nbSt_str);
+	nbAr = atoi(nbAr_str);
+
+	initGraph(g, nbSt);
+
+    printf("Nombre de sommets : %d\n", nbSt);
+	printf("Nombre d'aretes : %d\n", nbAr);
+
+	while(fscanf(source, "%s %s", from_str, to_str) != EOF){
+		from = atoi(from_str);
+		to = atoi(to_str);
+
+		if(g->a[from][to] != 1 && g->a[to][from] != 1){
+			g->degre[from] = g->degre[from] + 1;
+			g->degre[to] = g->degre[to] + 1;
+
+		}
+		if(g->a[from][to] != 1){
+			g->a[from][to] = 1;
+		}
+
+		if(g->a[to][from] != 1){
+			g->a[to][from] = 1;
+		}
+
+
+
+
+	}
+
+	fclose(source);
+}
+
+void printf_liste(liste x){
+
+	liste tmp = x; 	int i = 0;
+	printf("[ ");
+	while(tmp != NULL){
+		printf("%d ",tmp->st);
+		tmp = tmp->suiv;
+		i++;
+	}
+	printf("], taille %d.\n",i);
+}
+
+//Ajoute un sommet a à la liste x.
+liste add_to_liste(liste x, sommet a){
+	liste tmp = malloc(sizeof(liste));
+	tmp->st = a;
+	tmp->suiv = x;
+	return tmp;
+}
+
+void free_liste(liste x){
+
+	while(x != NULL){
+		liste tmp = x;
+		x = x -> suiv;
+		free(tmp);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+//___________________________________________________________________________
 
 
 
@@ -11,22 +177,29 @@ int verification_maximalite(Graph *g, liste X){
 	liste tmp_parcour = X;
 
 	//Si X n'est pas un sous graphe desert de G, il ne peut être maximal.
-	if(!verification_graphe_desert(g,X))
+	/*if(!verification_graphe_desert(g,X))
 		return 0;
-
+*/
 	for(int i=0;i<g->n;i++)
 			tableau_adjacence[i] = 1;
 
   //On parcourt X.
 	while (tmp_parcour != NULL) {
 
-  	//On marque le sommet choisi.
-		tableau_adjacence[tmp_parcour->st] = 0;
+  	//On marque les sommets choisis avec un entier négatif.
+		tableau_adjacence[tmp_parcour->st] = -1;
 
-  	//On marque ensuite tout ses adjacents.
+  	//On marque ensuite tout ses adjacents avec 0.
 		for(int i=0;i<g->n;i++){
-			if(g->a[tmp_parcour->st][i])
-				tableau_adjacence[i] = 0;
+			if(g->a[tmp_parcour->st][i]){
+
+					//Sauf si l'un des adjacents est déjà dans l'ensemble de sommets, auquel cas X n'est pas désert.
+					if(tableau_adjacence[i] == -1){
+						printf("X non désert, %d et %d adjacents et tous deux dans X.\n",tmp_parcour->st,i);
+						return 0;
+					}
+					else tableau_adjacence[i] = 0;
+			}
 		}
 		tmp_parcour = tmp_parcour -> suiv;
 	}
@@ -34,9 +207,9 @@ int verification_maximalite(Graph *g, liste X){
 	//  On parcourt notre tableau d'adjacence.
   //  Si l'on trouve un sommet non-marqué, càd pour lequel la valeur dans le tableau d'adjacence vaut 1, alors il existe un sommet que l'on peut rajouter à X : X n'est pas maximal
 	for(int i=0;i<g->n;i++)
-	  	if(tableau_adjacence[i]){
-			     printf("tab[%d] = %d\n",i, tableau_adjacence[i]);
-			   max = 0;
+	  	if(tableau_adjacence[i] > 0){
+			  	printf("tab[%d] = %d\n",i, tableau_adjacence[i]);
+			   	max = 0;
 		  }
 	return max;
 }
@@ -47,11 +220,11 @@ int verification_maximalite(Graph *g, liste X){
 
 int main(int argc, char *argv[]){
 
-    if(argc != 1){
-      printf("USAGE : ./verif_desert fichier_contenant_le_graphe\n");
+    if(argc != 2){
+      printf("USAGE : ./verif_maximal fichier_contenant_le_graphe\n");
       exit(-1);
     }
-    printf("argv[1] = %s\n",argv[1]);
+
     //Création du graphe.
     Graph *graph = malloc(sizeof(Graph));
     char* fgraph = argv[1];
@@ -59,19 +232,19 @@ int main(int argc, char *argv[]){
 
 
     //Exemple de création d'un sous-ensemble de sommets à tester :
-    print("Liste créée\n");
+
     liste sous_graphe_desert = NULL;
     sous_graphe_desert = add_to_liste(sous_graphe_desert,0);
     sous_graphe_desert = add_to_liste(sous_graphe_desert,2);
-    sous_graphe_desert = add_to_liste(sous_graphe_desert,4);
-
+    sous_graphe_desert = add_to_liste(sous_graphe_desert,5);
+		sous_graphe_desert = add_to_liste(sous_graphe_desert,7);
 
     //Résultat
     if(verification_maximalite(graph,sous_graphe_desert)){
-      printf("Le sous-graphe est maximal !\n");
+      printf("Le sous-graphe X est maximal !\n");
     }
     else {
-      printf("Le sous-graphe n'est pas maximal.\n");
+      printf("Le sous-graphe X n'est pas maximal.\n");
     }
 
     return 0;
